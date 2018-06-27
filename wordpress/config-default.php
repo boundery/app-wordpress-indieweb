b938ea409a12f026225ea538fb3c1653c9f6fcba
Nolan Leake <nolan@sigbus.net>
2018-06-27 15:23:09 -0700

    First working version.
    This installs apache, wordpress, the indieweb plugin, and an indieweb friendly theme.
    
    It doesn't activate the theme or the plugin yet.

---
+++ wordpress/config-default.php
@@ -0,0 +1,8 @@
+<?php
+#XXX Need to persist DB somewhere.
+define('DB_DIR', '/tmp/');
+define('DB_FILE', 'wp.sqlite3');
+define('USE_MYSQL', false);
+
+define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
+?>
